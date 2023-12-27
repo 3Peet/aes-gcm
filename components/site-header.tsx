60ecdf0f842e6104cc1897ef3efcb5b4149aca70
Thanakrit Naprasert <thanakrit.nap@satangcorp.com>
2023-12-27 09:43:43 +0700

    🧑‍💻 Add prettier eslint formatter

---
+++ components/site-header.tsx
@@ -0,0 +1,9 @@
+export default function SiteHeader() {
+  return (
+    <header className="border-border/40 bg-background/95 supports-[backdrop-filter]:bg-background/60 sticky top-0 z-50 w-full border-b backdrop-blur">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        header
+      </div>
+    </header>
+  )
+}
